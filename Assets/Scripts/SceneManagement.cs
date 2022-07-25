2507898ca69797aaef3938d72dd81da72e52d9f8
Trandafir Adrian-Stefan <trandafiradrian98@gmail.com>
2022-07-25 12:00:41 +0300

    Initial commit

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void HighscorePanel()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SettingsPanel()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void StatisticsPanel()
+    {
+        SceneManager.LoadScene(5);
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Settings : MonoBehaviour
+{
+    [Header("Volume Settings")]
+    [SerializeField] private Text _volumeTextValue = null;
+    [SerializeField] private Slider _volumeSlider = null;
+    [SerializeField] private float _defaultVolume = 1.0f;
+
+    [Header("Gameplay Settings")]
+    [SerializeField] private Text _controllerSensTextValue = null;
+    [SerializeField] private Slider _controllerSensSlider = null;
+    [SerializeField] private int _defaultSens = 4;
+    public int mainControllerSens = 4;
+
+    [Header("Toggle Settings")]
+    [SerializeField] private Toggle _invertYToggle = null;
+
+    [Header("Graphics Settings")]
+    [SerializeField] private Slider _brightnessSlider = null;
+    [SerializeField] private Text _brightnessTextValue = null;
+    [SerializeField] private float _defaultBrightness = 1.0f;
+
+    [Space(10)]
+    [SerializeField] private TMP_Dropdown _qualityDropdown;
+    [SerializeField] private Toggle _fullScreenToggle;
+
+    private int _qualityLevel;
+    private bool _isFullscreen;
+    private float _brightnessLevel;
+
+    [Header("UI")]
+    [SerializeField] private GameObject _confirmationMessage;
+
+    [Header("Resolution Dropdowns")]
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] _resolutions;
+
+    void Start()
+    {
+        _resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            string option = _resolutions[i].width + " x " + _resolutions[i].height;
+            options.Add(option);
+
+            if (_resolutions[i].width == Screen.width && _resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }    
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resultionIndex)
+    {
+        Resolution resolution = _resolutions[resultionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        _volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationMessage());
+    }
+
+    public void SetControllerSens(float sensitivity)
+    {
+        mainControllerSens = Mathf.RoundToInt(sensitivity);
+        _controllerSensTextValue.text = sensitivity.ToString("0");
+    }
+
+    public void GameplayApply()
+    {
+        if (_invertYToggle.isOn)
+        {
+            PlayerPrefs.SetInt("masterInvertY", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("masterInvertY", 0);
+        }
+        PlayerPrefs.SetFloat("masterSen", mainControllerSens);
+        StartCoroutine(ConfirmationMessage());
+    }
+
+    public void SetBrightness(float brightness)
+    {
+        _brightnessLevel = brightness;
+        _brightnessTextValue.text = brightness.ToString("0.0");
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        _isFullscreen = isFullscreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        _qualityLevel = qualityIndex;
+    }
+
+    public void GraphicsApply()
+    {
+        PlayerPrefs.SetFloat("masterBrightness", _brightnessLevel);
+
+        PlayerPrefs.SetInt("masterQuality", _qualityLevel);
+        QualitySettings.SetQualityLevel(_qualityLevel);
+
+        PlayerPrefs.SetInt("masterFullscreen", (_isFullscreen ? 1 : 0));
+        Screen.fullScreen = _isFullscreen;
+
+        StartCoroutine(ConfirmationMessage());
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Graphics")
+        {
+            _brightnessSlider.value = _defaultBrightness;
+            _brightnessTextValue.text = _defaultBrightness.ToString("0.0");
+
+            _qualityDropdown.value = 1;
+            QualitySettings.SetQualityLevel(1);
+
+            _fullScreenToggle.isOn = false;
+            Screen.fullScreen = false;
+
+            Resolution currentResolution = Screen.currentResolution;
+            Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
+            resolutionDropdown.value = _resolutions.Length;
+            GraphicsApply();
+        }
+
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = _defaultVolume;
+            _volumeSlider.value = _defaultVolume;
+            _volumeTextValue.text = _defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+
+        if(MenuType == "Gameplay")
+        {
+            _controllerSensTextValue.text = _defaultSens.ToString("0");
+            _controllerSensSlider.value = _defaultSens;
+            mainControllerSens = _defaultSens;
+            _invertYToggle.isOn = false;
+            GameplayApply();
+        }
+    }
+
+    public IEnumerator ConfirmationMessage()
+    {
+        _confirmationMessage.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        _confirmationMessage.SetActive(false);
+    }
+
+}
